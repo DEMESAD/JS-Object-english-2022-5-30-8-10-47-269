0be0897125129a7565aae3cdcefac77dc4eb3b20
DEMESAD <adrian.kyle.de.mesa@oocl.com>
2022-08-18 11:17:44 +0800

    js file added

---
+++ Object2.js
@@ -0,0 +1,7 @@
+var fruit = {
+    apple: 20,
+    pear: 20,
+    peach: 10,
+};
+var total = fruit.apple + fruit.pear + fruit.peach;
+document.write(total)
---
+++ obeject1.js
@@ -0,0 +1,8 @@
+
+    var user = {
+        surname: Mike,
+        };
+
+        
+
+
